194eced5fbd46b7ca0f0268ccc038349578ebe92
Lenny <valentin805@gmail.com>
2019-03-21 16:26:17 +0200

    Use UserService to register user in UserController.

--- server/src/main/java/project/areas/users/controller/UserController.java
+++ server/src/main/java/project/areas/users/controller/UserController.java
@@ -3,6 +3,7 @@ package project.areas.users.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import project.areas.users.entities.Role;
 import project.areas.users.models.bidingModels.UserRegisterForm;
 import project.areas.users.services.RoleService;
 import project.areas.users.services.UserService;
@@ -28,8 +29,8 @@ public class UserController {
     public ResponseEntity registerUser(@RequestBody final UserRegisterForm registerForm){
         System.out.println(registerForm.getEmail());
         System.out.println(registerForm.getPass());
-
-
+        Role role = this.roleService.getRoleByName("ROLE_NORMAL");
+        this.userService.registerUser(registerForm,role);
         return ResponseEntity.ok("Successful sign-up");
     }
 }
