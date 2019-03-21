dd3a0e840d6b5651dc8475a7c2171137739d9ff9
Lenny <valentin805@gmail.com>
2019-03-21 15:40:06 +0200

    Create an User entity.

---
+++ server/src/main/java/project/areas/users/entities/User.java
@@ -0,0 +1,45 @@
+package project.areas.users.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "users")
+public class User {
+    private Integer id;
+    private String email;
+    private String password;
+
+    public User(){
+
+    }
+
+    public User(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
