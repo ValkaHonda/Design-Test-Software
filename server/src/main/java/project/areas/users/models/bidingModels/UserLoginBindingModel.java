6b18dfbd4b12a04de8660b9f425f3823b5dad208
Lenny <valentin805@gmail.com>
2019-03-21 17:51:19 +0200

    Create an UserLoginBindingModel.

---
+++ server/src/main/java/project/areas/users/models/bidingModels/UserLoginBindingModel.java
@@ -0,0 +1,32 @@
+package project.areas.users.models.bidingModels;
+
+public class UserLoginBindingModel {
+    private String username;
+    private String password;
+    private Boolean isRememberMe;
+
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Boolean getRememberMe() {
+        return isRememberMe;
+    }
+
+    public void setRememberMe(Boolean rememberMe) {
+        isRememberMe = rememberMe;
+    }
+}
