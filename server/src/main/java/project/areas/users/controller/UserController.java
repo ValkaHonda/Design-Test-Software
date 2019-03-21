1bc9660695fdd4934ecb0470187d447c89c93490
Lenny <valentin805@gmail.com>
2019-03-21 16:23:08 +0200

    Autowire User & Role services to User Controller.

--- server/src/main/java/project/areas/users/controller/UserController.java
+++ server/src/main/java/project/areas/users/controller/UserController.java
@@ -1,13 +1,25 @@
 package project.areas.users.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import project.areas.users.models.bidingModels.UserRegisterForm;
+import project.areas.users.services.RoleService;
+import project.areas.users.services.UserService;
 
 @RestController
 @CrossOrigin(origins = "http://localhost:3000")
 @RequestMapping("/users")
 public class UserController {
+    private UserService userService;
+    private RoleService roleService;
+
+    @Autowired
+    public UserController(final UserService userService, final RoleService roleService) {
+        this.userService = userService;
+        this.roleService = roleService;
+    }
+
     @GetMapping("/test")
     public String test(){
         return "<h1>Hello from UserController.<h1>";
@@ -16,6 +28,7 @@ public class UserController {
     public ResponseEntity registerUser(@RequestBody final UserRegisterForm registerForm){
         System.out.println(registerForm.getEmail());
         System.out.println(registerForm.getPass());
+
 
         return ResponseEntity.ok("Successful sign-up");
     }
